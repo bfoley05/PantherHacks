9b7a53f030aa79015548cd983b35770404176f52
Brandon Foley <brfoley@chapman.edu>
2026-04-03 18:55:30 -0700

    Inital Commit

---
+++ Venture Local/Venture Local/Venture_LocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Venture_LocalApp.swift
+//  Venture Local
+//
+//  Created by Brandon Foley on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Venture_LocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
